a10453e95b901133875bc5c62394348013628afb
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-04 10:37:43 +0900

    Revert "Revert "第1章 指摘されていた分の訂正が終了しました。 ファイルヘッダを追加し、演習の概要を書き込みました""
    This reverts commit 706f8e51e3c77a8bacfaf388e2ae93359c3a8c0c.

---
+++ 第1章/第1章/演習1-12/演習1-12.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+
+using namespace std;
+
+// プログラム本文始まり
+int main()
+{
+	double hen,takasa;					// double型 hen takasa それぞれ 底辺 高さ
+
+	// 三角形の面積を求めるために宣言を行う
+	cout << "三角形の面積を求めます。\n";
+	// 三角形の面積を求めるために 底辺 の 値を 抽出する部分
+	cout << "底辺 : ";
+	// cin で代入する値を抽出する
+	cin >> hen;
+	// 三角形の面積を求めるために 高さ の 値を 抽出する部分
+	cout << "高さ : ";
+	// cin で代入する値を抽出する
+	cin >> takasa;
+
+	double menseki = (hen*takasa)/2;	//三角形の面積を求めるために double型で変数を定義し
+										//値を三角形の面積の公式で初期化
+
+	//挿入子で変数 menseki の三角形の面積が表紙さる
+	cout << "面積は" << menseki << "です。";
+
+	return 0;
+
+}
