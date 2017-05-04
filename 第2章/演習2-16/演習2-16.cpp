931b439ba5d85fb79179e7beed9b5ac5aaecfcde
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-04 15:46:18 +0900

    整数値を入力するように変更しました。 範囲外の数値のときのdefaultを追加しました ファイルヘッダを追加しました。

--- 第2章/演習2-16/演習2-16.cpp
+++ 第2章/演習2-16/演習2-16.cpp
@@ -1,53 +1,54 @@
-//演習2-16 switch文で月を季節に変換する処理
+/*
+演習2-16 switch文で月を季節に変換する処理
 
+作成日 2017年5月4日
+
+作成者 平澤敬介
+*/
 #include<iostream>
-// ランダム関数を定義しているライブラリ
-#include<cstdlib>
-// 現在時刻の情報を持ったライブラリ
-#include<ctime>
 
 using namespace std;
 
 int main()
 {
-	// 現在時刻を使って乱数の種を生成する
-	srand(time(NULL));
+	int nselect;	// 整数を読み込み季節を返すための変数を定義する
 
-	// 乱数 0～ 11の間で発生
-	int n = rand() % 12;
-
-	// n をインクリメントすることで乱数が 1 ～ 12 の間の乱数になる
-	n++;
+	//整数を入力するように促す 1から12以外の数字でも構わない
+	cout << "1～12の整数を入力してください :";
+	//変数nselectにキーボードからの値を代入する
+	cin >> nselect;
 
 	// まずは、n月を表示する 後に季節が表示されるのであっているか確認できる
-	cout << n << "月\n";
+	cout << nselect << "月ですね\n";
 
 	// 月の数でswitch文に入る 応じた季節を出力する
-	switch(n){
+	switch(nselect){
 	// １月 通過
 	case(1) :
 	// 1月 2月は冬なので 冬と表示して break switch文から脱出
-	case(2) : cout << "冬\n"; break;
+	case(2) : cout << "冬です\n"; break;
 	// ３月 通過
 	case(3) :
 	// ４月 通過
 	case(4) :
 	//  3月 4月 5月は春なので 春と表示して break switch文から脱出
-	case(5) : cout << "春\n"; break;
+	case(5) : cout << "春です\n"; break;
 	// ６月 通過
  	case(6) :
 	// ７月 通過
 	case(7) :
 	//  6月 7月 8月は夏なので 夏と表示して break switch文から脱出
-	case(8) : cout << "夏\n"; break;
+	case(8) : cout << "夏です\n"; break;
 	// ９月 通過
 	case(9) :
 	// 1０月 通過
 	case(10) :
 	//  9月 10月 11月は秋なので 秋と表示して break switch文から脱出
-	case(11) : cout << "秋\n"; break;
+	case(11) : cout << "秋です\n"; break;
 	//  12月は冬なので 冬と表示して break switch文から脱出
-	case(12) : cout << "冬\n"; break;
+	case(12) : cout << "冬です\n"; break;
+	// 1～12以外の数字が入力された場合、季節を表示せずswitch文から出る
+	default : cout << "その月はありません\n"; break;
 	}
 	return 0;
 }
