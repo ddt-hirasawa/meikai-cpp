50470b47c30da4ab67e9bc9e8fee863103bbc7d0
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-04 11:07:06 +0900

    演習2-2 演習2-3 のファイルの追加と訂正が完了しました。

--- 第2章/演習2-3/演習2-3.cpp
+++ 第2章/演習2-3/演習2-3.cpp
@@ -1,26 +1,32 @@
-//演習2-3 整数の比較を行い 大きいほうの値を表示する
-// もしくは、等しいことを告げる処理
-
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
-	//
-	int a,b;// 変数の定義 a,bそれぞれキーボードから読み込むため、初期化する必要なし
+	int sample1,sample2;// 変数の定義 sample1,sample2それぞれキーボードから読み込むため、初期化する必要なし
 
-	// a に代入する値を決める部分 0 正の値 負の値 整数ならば 構わない
-	cout << "整数値a : "; cin >> a;
-	// b に代入する値を決める部分 0 正の値 負の値 整数ならば 構わない
-	cout << "整数値b : "; cin >> b;
+	// sample1 に代入する値を決める部分 0 正の値 負の値 整数ならば 構わない
+	cout << "整数値 sample1 : ";
+	// cin でsample1 の変数に代入を行う
+	cin >> sample1;
+	// sample2 に代入する値を決める部分 0 正の値 負の値 整数ならば 構わない
+	cout << "整数値 sample2 : ";
+	// cin でsample2 の変数に代入を行う
+	cin >> sample2;
 
-	// 仮に a = 5 b = 4 ならば a と b が比較され 5のほうが大きい と表示される
-	if(a>b){ cout << "aのほうが大きいですよ。";
-	// 仮に a = 3 b = 10 ならば a と b が比較され 10のほうが大きい と表示される
-	}else if(a<b){	 cout << "bのほうが大きいですよ。";
-	// 仮に a = 5 b = 5 ならば a と b が比較され 同じ値 と表示される
-	}else if(a==b){ cout << "aとbは同じ値です。";
+		// 仮に sample1 = 5 sample2 = 4 ならば sample1 と sample2 が比較され 5のほうが大きい と表示される
+	if(sample1 > sample2) {
+		// 最初に入力した値が大きい場合のときの表示を行う
+		cout << "sample1のほうが大きいですよ。";
+		// 仮に sample1 = 3 sample2 = 10 ならば sample1 と sample2 が比較され 10のほうが大きい と表示される
+	} else if(sample1 < sample2) {
+		// 後に入力した値が大きい場合のときの表示を行う
+		cout << "sample2のほうが大きいですよ。";
+		// 仮に sample1 = 5 sample2 = 5 ならば sample1 と sample2 が比較され 同じ値 と表示される
+	} else if(sample1 == sample2) {
+		// 最初に入力した値と後に入力した値が等しいときの表示を行う
+		cout << "sample1とsample2は同じ値です。";
 	}
 	return 0;
 }
