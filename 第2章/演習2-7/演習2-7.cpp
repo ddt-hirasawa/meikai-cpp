942e9d43f9273b2ed86fccd2b17987cb2993e385
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-04 11:58:54 +0900

    演習2-7 変数maxを作成し値を代入するようにしました

--- 第2章/演習2-7/演習2-7.cpp
+++ 第2章/演習2-7/演習2-7.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main()
 {
 	int sample1,sample2;// 比較する変数の定義 両方キーボードから読み込むため 初期化はいらない
+	int max;			// 大きい値を格納する変数を定義します
 
 		// 実数であるならば どの値でも受け付ける 変数を代入する部分
 	cout << "整数 sample1 :";
@@ -16,10 +17,12 @@ int main()
 		// cin で sample2に値を代入する
 	cin >> sample2;
 
-	// 条件演算子を使った場合、cout の中に条件演算子を組み込み
-	// sample1 が sample2より大きい場合 true となり 左側の値が
-	// そうでない場合 false となり 右側の値が表示されます
-	cout << "大きい値は" << (sample1 >= sample2 ? sample1 : sample2) << "です。";
+
+	max = (sample1 >= sample2 ? sample1 : sample2);	// 条件演算子を使った場合、cout の中に条件演算子を組み込み
+													// sample1 が sample2より大きい場合 true となり 左側の値が
+													// そうでない場合 false となり 右側の値が代入されます
+		// maxに代入された値は sample1,2の大きいほうの値です
+	cout << "大きい値は " << max << "です。";
 
 	/* 変更前
 		// aとbが比較され 仮に sample1 = 5 sample2 = -1 ならば sample1 が大きいと判断される
