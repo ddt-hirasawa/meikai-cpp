85e664c7bb05e53a103fcf10ab9f082278de3840
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-05 17:47:24 +0900

    コメントの修正、ファイルヘッダの追加を行いました

--- e_03_26/src/e_03_26.cpp
+++ e_03_26/src/e_03_26.cpp
@@ -1,67 +1,231 @@
-#include<iostream> // 入出力のヘッダ	3回勝たなければ終了しません
-#include<ctime>	// 現在時刻
-#include<cstdlib> // ランダム関数
+/*
+演習3-26 じゃんけんプログラムの作成
 
-using namespace std;// 名前空間の指定
+作成日 2017年5月5日
 
-int main()// main文の始まり
+作成者 平澤敬介
+*/
+
+#include<iostream>
+
+	// 現在時刻を定義しているライブラリ
+#include<ctime>
+
+ // ランダム関数を定義しているライブラリ
+#include<cstdlib>
+
+using namespace std;
+
+
+int main()
 {
-	int mine;	// 自分の手
-	int com;	// 相手の手
-	int m = 0; // 自分の勝利回数
-	int c = 0; //相手の勝利回数
-	do{
-	do{
-		cout << "入力してください\n"					//コンソールに表示
-		<< "グー・・・0 チョキ・・・1 パー・・・2\n";	//コンソールに表示
-		cin >> mine; 									// 自分の手
-		if(mine < 0 || mine > 2)						// 0～２の間でなければ
-			cout << "入力ミス\n";						//コンソールに表示
-	}while(mine < 0 || mine > 2);						// 0～２の間で入力されるまで
+	int mine;	//	 自分の手
+	int com;	//	 相手の手
+	int mycnt = 0; // 	自分の勝利回数
+	int comcnt = 0; //	相手の勝利回数
 
-	switch(mine){										// mine の 値で caseに向かう
-	case(0) : cout << "自分 : グー\n"; break;			// mine = 0のとき
-	case(1) : cout << "自分 : チョキ\n"; break;		// mine = 1のとき
-	case(2) : cout << "自分 : パー\n"; break;			// mine = 2のとき
-	}													// switch文終わり
+	// ゲーム開始
+	do {
+		// 自分の手を決める部分
+		do {
+			// 0～2の間で入力を受けつけます
+			cout << "入力してください\n"
+					<< "グー・・・0 チョキ・・・1 パー・・・2\n";
+			// キーボードからの値をmineに代入します
+			cin >> mine;
 
-	srand(time(NULL));					// 乱数の生成
-	com = rand() % 3; 					// 相手の手
-	switch(com){										// com の 値で caseに向かう
-	case(0) : cout << "COM : グー\n"; break;			// com = 0 のとき
-	case(1) : cout << "COM : チョキ\n"; break;			// com = 1 のとき
-	case(2) : cout << "COM : パー\n"; break;			// com = 2 のとき
-	}													// switch文終わり
+			//グー チョキ パーいずれでもなければ
+			if (mine < 0 || mine > 2) {
+				//入力ミス扱いにしてやり直し
+				cout << "入力ミス\n";
+			}
+			// グー チョキ パーが入力されたのならば終了します
+		} while (mine < 0 || mine > 2);
 
-	if(mine == 0){										// 自分がグーのとき
-		switch(com){									// com の値で caseに向かう
-		case(0) : cout << "引き分けです\n"; break;		// 引き分け
-		case(1) : cout << "勝ちです\n";m++; break;		// m をインクリメント
-		case(2) : cout << "	負けです\n";c++; break;		// c をインクリメント
-		}													// switch文終わり
-	}
-	if(mine == 1){										// 自分がチョキのとき
-		switch(com){									// com の値で caseに向かう
-		case(0) : cout << "負けです\n";c++; break;		// c をインクリメント
-		case(1) : cout << "引き分けです\n"; break;			// 引き分け
-		case(2) : cout << "	勝ちです\n";m++; break;		// m をインクリメント
-		}													// switch文終わり
-	}
-	if(mine == 2){										// 自分がパーのとき
-		switch(com){									// com の値で caseに向かう
-		case(0) : cout << "勝ちです\n";m++; break;		// m をインクリメント
-		case(1) : cout << "負けです\n";c++; break;		// c をインクリメント
-		case(2) : cout << "	引き分けです\n"; break;			// 引き分け
-		}													// switch文終わり
-	}
-	cout << "YOU :" << m << "回\n"<< "COM :" << c << "回\n"; // 勝利回数を表示
-	}while(m < 3 && c < 3);							// 自分か相手が３回勝つまでループ
+		// 自分の手を表示する部分
+		switch (mine) {
+		// mine = 0のとき
+		case (0):
+			// グーを出しました
+			cout << "自分 : グー\n";
+			// switchから抜けます
+			break;
 
-	if(m == 3)										// 自分が３回勝ったなら
-		cout << "YOU_WIN\n";						// 自分の勝ち
-	else											// 違うのならば
-		cout << "YOU_LOSE\n";						// 自分の負け
+		// mine = 1のとき
+		case (1):
+			// チョキを出しました
+			cout << "自分 : チョキ\n";
+		// switchから抜けます
+			break;
+
+		// mine = 2のとき
+		case (2):
+
+			// パーを出しました
+			cout << "自分 : パー\n";
+		// switchから抜けます
+			break;
+		}
+		// 乱数の生成
+		srand(time(NULL));
 
 
-	return 0;		// プログラム終了
-} // main文終わり
+		com = rand() % 3; 	// 相手の手を乱数で決めます
+
+		// com の 値で グー チョキ パー を決めます
+		switch (com) {
+
+		// com = 0 のとき
+		case (0):
+			// グーを出しました
+			cout << "COM : グー\n";
+		// switchから抜けます
+			break;
+
+		// com = 1 のとき
+		case (1):
+			// チョキを出しました
+			cout << "COM : チョキ\n";
+		// switchから抜けます
+			break;
+
+		// com = 2 のとき
+		case (2):
+			// パーを出しました
+			cout << "COM : パー\n";
+		// switchから抜けます
+			break;
+		}
+
+		// 自分がグーのとき
+		if (mine == 0) {
+
+			// com の値で caseに向かう
+			switch (com) {
+
+			// 相手はグーです
+			case (0):
+
+				// 両方のカウントを行いません
+				cout << "引き分けです\n";
+			// switchから抜けます
+				break;
+
+			// 相手はチョキです
+			case (1):
+
+				// 勝ったので自分のカウントを行います
+				cout << "勝ちです\n";
+
+				// 自分のカウントをインクリメント
+				mycnt++;
+				// switchから抜けます
+				break;
+
+			// 相手はパーです
+			case (2):
+
+				//負けたので相手のカウントを行います
+				cout << "負けです\n";
+
+				//相手のカウントをインクリメント
+				comcnt++;
+				// switchから抜けます
+				break;
+			}
+		}
+
+		// 自分がチョキのとき
+		if (mine == 1) {
+
+			// com の値で caseに向かう
+			switch (com) {
+
+			// 相手はグーです
+			case (0):
+
+				//負けたので相手のカウントを行います
+				cout << "負けです\n";
+
+				//相手のカウントをインクリメント
+				comcnt++;
+				// switchから抜けます
+				break;		// c をインクリメント
+
+			// 相手はチョキです
+			case (1):
+
+			// 両方のカウントを行いません
+				cout << "引き分けです\n";
+			// switchから抜けます
+				break;
+
+			// 相手はパーです
+			case (2):
+
+				// 勝ったので自分のカウントを行います
+				cout << "勝ちです\n";
+
+				// 自分のカウントをインクリメント
+				mycnt++;
+				// switchから抜けます
+				break;
+			}
+		}
+
+		// 自分がパーのとき
+		if (mine == 2) {
+
+			// com の値で caseに向かう
+			switch (com) {
+
+			// 相手はグーです
+			case (0):
+
+				// 勝ったので自分のカウントを行います
+				cout << "勝ちです\n";
+				// 自分のカウントをインクリメント
+				mycnt++;
+				// switchから抜けます
+				break;
+
+			// 相手はチョキです
+			case (1):
+
+				//負けたので相手のカウントを行います
+				cout << "負けです\n";
+
+				//相手のカウントをインクリメント
+				comcnt++;
+				// switchから抜けます
+				break;
+
+			// 相手はパーです
+			case (2):
+
+				// 両方のカウントを行いません
+				cout << "	引き分けです\n";
+			// switchから抜けます
+				break;
+			}
+		}
+		// 勝利回数を表示	自分とCOMの勝った回数を表示していく
+		cout << "YOU :" << mycnt << "回\n" << "COM :" << comcnt << "回\n";
+
+		// 自分か相手が３回勝つまでループ
+	} while (mycnt < 3 && comcnt < 3);
+
+
+	// 自分が３回勝ったなら
+	if (mycnt >= 3)
+
+		// 自分の勝ち
+		cout << "YOU_WIN\n";
+	// 3回勝ってないのに終了したら
+	else
+		// 自分の負け
+		cout << "YOU_LOSE\n";
+
+	return 0;
+
+}
