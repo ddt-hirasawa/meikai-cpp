ea3cb3f6dfbd45d92ee1821056e08cec85197017
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-12 19:27:30 +0900

    演習14-3 終了しました

--- e_14_03/src/e_14_03.cpp
+++ e_14_03/src/e_14_03.cpp
@@ -21,35 +21,39 @@ int main()
 	//クラスオブジェクト 人間で生成
 	String human = "human_man";
 
+	cout << "変換コンストラクタ \n";
 	//クラスに代入した時の名前が表示されるかの確認
-	cout << human.open_name() << "\n";
+	cout << "名前 : " << human.open_name() << "\n";
 	//文字数が出せるのかの確認
-	cout << human.open_len() << "\n";
+	cout << "名前の全文字数 : " << human.open_len() << "\n";
 	//中間文字を出せるのかの確認
-	cout << human.med_length() << "\n";
+	cout << "名前の中間文字 : " << human.med_length() << "\n";
 
 	//コピーコンストラクタの動作確認
 	//クラスオブジェクト 人間1で生成
 	String human1 = human;
 
+	cout << "コピーコンストラクタ \n";
 	//クラスに代入した時の名前が表示されるかの確認
-	cout << human1.open_name() << "\n";
+	cout << "名前 : " << human1.open_name() << "\n";
 	//文字数が出せるのかの確認
-	cout << human1.open_len() << "\n";
+	cout << "名前の全文字数 : " << human1.open_len() << "\n";
 	//中間文字を出せるのかの確認
-	cout << human1.med_length() << "\n";
+	cout << "名前の中間文字 : "<< human1.med_length() << "\n";
 
 	//明示的コンストラクタの動作確認
 	//クラスオブジェクト 人間で生成
 	String human2(ptr);
 
+	cout << "明示的コンストラクタ \n";
 	//クラスに代入した時の名前が表示されるかの確認
-	cout << human2.open_name() << "\n";
+	cout << "名前 : " << human2.open_name() << "\n";
 	//文字数が出せるのかの確認
-	cout << human2.open_len() << "\n";
+	cout << "名前の全文字数 : " << human2.open_len() << "\n";
 	//中間文字を出せるのかの確認
-	cout << human2.med_length() << "\n";
+	cout << "名前の中間文字 : " << human2.med_length() << "\n";
 
+	cout << "演算子関数 + により大文字にします\n";
 	human2 + 8;		//演算子関数 + の動作確認 大文字に変換します
 
 	//参照しているので、データメンバ自体を書き換えます
@@ -57,16 +61,24 @@ int main()
 
 	char* another = new char[30];		//他人の名前を書くための用紙を用意します
 
-	cout << "あなたは誰ですか？ : ";	//誰かの名前を入力してください
+	//誰かの名前を入力してください
+	cout << "あなたは誰ですか？ : ";
 
-	cin >> another;						//キーボードからのアルファベットで入力
+	//キーボードからのアルファベットで入力
+	cin >> another;
 
+	cout << "演算子関数 = により代入し名前を書き換えます\n";
 	human2 = another;					//演算子関数 + の動作確認 大文字に変換します
 
-	delete[] another;					//代入し終わったので 他人ではなく本人になりました
+	//代入し終わったので 他人ではなく本人になりました
+	delete[] another;
 
 	//参照しているので、データメンバ自体を書き換えます
-	cout << human2.open_name() << "\n";
+	cout << "名前 : " << human2.open_name() << "\n";
+	//文字数が出せるのかの確認
+	cout << "名前の全文字数 : " << human2.open_len() << "\n";
+	//中間文字を出せるのかの確認
+	cout << "名前の中間文字 : " << human2.med_length() << "\n";
 
 	return 0;
 }
