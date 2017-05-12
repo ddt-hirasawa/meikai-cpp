3b2f38fc836dca69199afe32bab565b40ccc40ec
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-12 20:03:40 +0900

    デストラクタを忘れていたので追加します

--- e_14_03/src/class.h
+++ e_14_03/src/class.h
@@ -10,6 +10,7 @@
 #define CLASS_H_
 
 #include<cstring>
+#include<iostream>
 
 class String {
 
@@ -22,17 +23,15 @@ public:
 	explicit String(char* ptr_)
 
 	//名前の長さは cstring の関数でコンストラクタが呼ばれたとき代入する
-	: len(std::strlen(ptr_))
-	{
+	:
+			len(std::strlen(ptr_)) {
 		//初期化されたときの値をそのままデータメンバへ
 		ptr = ptr_;
 	}
-/*
-	//デストラクタ
-	~String() {
 
-		delete[] ptr;
-	}*/
+	 //デストラクタ
+	 ~String() {
+	 }
 
 	//データメンバ フルネームを表示します
 	char* open_name() const {
@@ -50,17 +49,17 @@ public:
 	//名前と苗字の中間の長さを表示します
 	int med_length() {
 
-		int judg=0;		//中間文字がなかったとき用
+		int judg = 0;		//中間文字がなかったとき用
 
 		int len = 0;	//先頭文字を指している状態で初期化
 
 		char* serch = ptr;	//データメンバ のポインタを借用
 
 		//名前の最後まで中間文字を走査します
-		for(; *serch++ != '\0'; len++) {
+		for (; *serch++ != '\0'; len++) {
 
 			//中間を発見しました
-			if(*serch == '_') {
+			if (*serch == '_') {
 
 				judg++;
 				//2個も3個もないのでループから出ます
@@ -71,7 +70,7 @@ public:
 		len++;		//1つ手前なので中間へ移動
 
 		//ただし、中間文字を検出できなかったときは
-		if(judg == 0) {
+		if (judg == 0) {
 
 			//中間無しということで、先頭に戻します
 			len = 0;
@@ -82,38 +81,31 @@ public:
 	}
 
 	//変換コンストラクタ
-	 String(const char* tmp)
-	 : ptr(const_cast<char*>(tmp))
-	 {
-		 //文字の長さを文字数で初期化
-		 len = std::strlen(tmp);
-	 }
+	String(const char* tmp) :
+			ptr(const_cast<char*>(tmp)) {
+		//文字の長さを文字数で初期化
+		len = std::strlen(tmp);
+	}
 
-	 //コピーコンストラクタ
-	 String(const String& tmp) {
+	//コピーコンストラクタ
+	String(const String& tmp) {
 
-		 len = tmp.len;			//コピー元の文字列の長さを覚えます
+		len = tmp.len;			//コピー元の文字列の長さを覚えます
 
-		 ptr = new char[len];	//その長さ文で領域を確保します
+		ptr = new char[len];	//その長さ文で領域を確保します
 
-		 //コピー開始 文字列分の代入を行います
-		 for(int i=0; i < len; i++) {
+		//コピー開始 文字列分の代入を行います
+		for (int i = 0; i < len; i++) {
 
-			 //自分と参照している相手は同じ人間です
-			 ptr[i] = tmp.ptr[i];
-		 }
-		 ptr[len] = '\0';		//文字化けするのでナル文字を入れます
-	 }
-
-	 /*~String() {
-
-
-	 delete[] ptr;
-	 }*/
+			//自分と参照している相手は同じ人間です
+			ptr[i] = tmp.ptr[i];
+		}
+		ptr[len] = '\0';		//文字化けするのでナル文字を入れます
+	}
 
 	//演算子関数の定義 func.cppにまとめます
-	String& operator + (int);
-	String& operator = (char*);
+	String& operator +(int);
+	String& operator =(char*);
 };
 
 #endif /* CLASS_H_ */
--- e_14_03/src/fanc.cpp
+++ e_14_03/src/fanc.cpp
@@ -1,10 +1,10 @@
 /*
-演習14-3 簡易的文字列クラスを作成せよ 自分で設計すること
+ 演習14-3 簡易的文字列クラスを作成せよ 自分で設計すること
 
-作成日 2017年5月12日
+ 作成日 2017年5月12日
 
-作成者 平澤敬介
-*/
+ 作成者 平澤敬介
+ */
 
 #include"class.h"
 #include<iostream>
@@ -14,27 +14,35 @@ using namespace std;
 
 //演算子関数 +
 //加算された数字で先頭から小文字を大文字に変換します
-String& String::operator + (int num) {
+String& String::operator +(int num) {
 
-	for(int i=0; i < num; i++) {
+	//指定した文字まで大文字に変換します
+	for (int i = 0; i < num; i++) {
 
-		this->ptr[i] = toupper(this -> ptr[i]);
+		//参照で今の文字を大文字に変換します
+		this->ptr[i] = toupper(this->ptr[i]);
 	}
-
+	//自分自身に反映させます
 	return *this;
 }
 
 //演算子関数 =
 //入力された文字でクラスのデータメンバ名前を書き換えます
-String& String::operator = (char* name) {
+String& String::operator =(char* name) {
 
-	int len = strlen(name);
+	int len1 = strlen(name);	//書き換える名前の文字数を出します
 
-	for(int i=0; i< len; i++) {
+	//その文字数文の書き換えを行います
+	for (int i = 0; i < len1; i++) {
 
-		this -> ptr[i] = name[i];
+		//自分自身を代入された文字に書き換えます
+		this->ptr[i] = name[i];
 	}
 
-	this -> ptr[len] = '\0';
+	this->ptr[len1] = '\0';		//文字化けするのでナル文字を置きます
+
+	this->len = len1;				//全文字数はコンストラクタで初期化されているので更新します
+
+	//自分自身に反映させます
 	return *this;
 }
